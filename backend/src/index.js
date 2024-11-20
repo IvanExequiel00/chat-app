4dba5b94696dc3aac6437dfc0d00fffeddb71b59
IvanRojas FT47b <exequieliv@outlook.es>
2024-11-20 16:30:42 -0300

    backend/src/index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 import express from "express"
 import authRoutes from "./routes/auth.route.js"
+import dotenv from "dotenv"
 
+dotenv.config();
 
-const PORT = 5001;
+const PORT = process.env.PORT;
 const app = express();
 
 app.use("/api/auth", authRoutes)
