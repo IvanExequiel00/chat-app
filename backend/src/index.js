547bdde8b20e0e2a3d5cdd54a3c2c7bc813a21a5
IvanRojas FT47b <exequieliv@outlook.es>
2024-11-20 16:16:54 -0300

    backend/src/index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,11 @@
 import express from "express"
+import authRoutes from "./routes/auth.route.js"
+
 
 const PORT = 5001;
 const app = express();
+
+app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
