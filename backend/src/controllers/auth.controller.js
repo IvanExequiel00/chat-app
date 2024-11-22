a2187b6f680c47042e7348346a26fef71dab7c68
IvanRojas FT47b <exequieliv@outlook.es>
2024-11-22 15:07:43 -0300

    backend/src/controllers/auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,17 @@
-export const signup = (req, res) =>{
-    res.send("singup route")
+import User from "../models/user.model.js"
+export const signup = async (req, res) =>{
+    const {fullName, email, password} = req.body
+    try {
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters" });
+          }
+
+          const user = await User.findOne({email})
+
+          if (user) return res.status(400).json({ message: "Email already exists" });
+    } catch (error) {
+        
+    }
 }
 
 export const login = (req, res) =>{
